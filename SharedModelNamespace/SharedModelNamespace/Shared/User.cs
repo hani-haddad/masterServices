5710476ef1c3f42f2220fd5b58092eafcf8c9f3c
Hani Haddad <hani@capella.io>
2023-10-22 09:13:58 +0300

    add shared model user

---
+++ SharedModelNamespace/SharedModelNamespace/Shared/User.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SharedModelNamespace.Shared
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("FirstName")]
+        public string FirstName { get; set; }
+
+        [BsonElement("LastName")]
+        public string LastName { get; set; }
+
+        [BsonElement("Email")]
+        public string Email { get; set; }
+
+        [BsonElement("Username")]
+        public string Username { get; set; }
+
+        [BsonElement("Password")]
+        public string Password { get; set; }
+
+        [BsonElement("Age")]
+        public string Age { get; set; }
+
+        [BsonElement("AdminOfGroups")]
+        public List<string> AdminOfGroups { get; set; }
+
+        [BsonElement("MemberInGroups")]
+        public List<string> MemberInGroups { get; set; }
+
+        [BsonElement("Image")]
+        public string Image { get; set; }
+
+        [BsonElement("SentInvitations")]
+        public List<string> SentInvitations { get; set; }
+
+        [BsonElement("RecivedInvitations")]
+        public List<string> RecivedInvitations { get; set; }
+
+    }
+}
